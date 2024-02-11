25098603af3f400bd95677616d846405aa9eb9db
federicoherrera <fede.juan.herrera@gmail.com>
2024-02-11 16:37:41 -0300

    commit

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,10 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors()
+  app.enableCors({ 
+    origin: 'https://nestauth-phi.vercel.app',
+    credentials: true
+  })
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
