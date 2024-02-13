4495a2f6263673be91963e204ff3b595499aa367
federicoherrera <fede.juan.herrera@gmail.com>
2024-02-13 13:37:35 -0300

    f

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -26,7 +26,11 @@ export class AuthController {
 
     @Get('profile')
     @UseGuards(AuthGuard)
-    profile(@Request() req) {
+    profile(@Request() req, @Res() res) {
+        res.header('Access-Control-Allow-Origin', 'https://nestauth-phi.vercel.app');
+        res.header('Access-Control-Allow-Methods', 'GET');
+        res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+        res.header('Access-Control-Allow-Credentials', 'true');
         return req.user
     }
 }
