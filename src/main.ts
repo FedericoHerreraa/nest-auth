bcf6066d4a6e8d1968672c2cc8b764c81a36cc79
federicoherrera <fede.juan.herrera@gmail.com>
2024-02-13 15:01:51 -0300

    f

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,11 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({ 
-    origin: 'https://nestauth-phi.vercel.app',
+    origin: ['https://nestauth-phi.vercel.app', 'http://localhost:5173'],
     credentials: true,
     allowedHeaders: ['Authorization', 'Content-Type'],
     exposedHeaders: 'Authorization',
-    methods: ['POST', 'PUT', 'DELETE', 'GET']
+    methods: ['POST','GET']
   })
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
