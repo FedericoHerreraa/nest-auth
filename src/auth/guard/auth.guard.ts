525becc511f0fdfab3adc31bc53f5cb07d8158ae
federicoherrera <fede.juan.herrera@gmail.com>
2024-02-13 11:49:01 -0300

    f

--- src/auth/guard/auth.guard.ts
+++ src/auth/guard/auth.guard.ts
@@ -27,10 +27,6 @@ export class AuthGuard implements CanActivate {
 
   private extractTokenFromHeader(request: Request): string | undefined {
     const authorizationHeader = request.headers.authorization;
-    console.log(authorizationHeader)
-    if (!authorizationHeader || !authorizationHeader.startsWith('Bearer ')) {
-        return undefined;
-    }
-    return authorizationHeader.substring(7);
+    return authorizationHeader
   }
 }
